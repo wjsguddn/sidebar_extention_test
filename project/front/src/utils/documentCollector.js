5db141e3b8f43167ca01e7bd71a4b6de97b1ff7f
wjsguddn98 <eric1208@naver.com>
2025-06-25 01:48:13 +0900

    add some comments

--- project/front/src/utils/documentCollector.js
+++ project/front/src/utils/documentCollector.js
@@ -1,5 +1,4 @@
-
 export function documentCollector() {
-  // TODO: PDF, DOC 등에서 본문 텍스트 추출
+  // PDF, DOC 등에서 본문 텍스트 추출
   return {};
 } 
--- project/front/src/utils/youtubeCollector.js
+++ project/front/src/utils/youtubeCollector.js
@@ -1,4 +1,4 @@
 export function youtubeCollector() {
-  // TODO: 유튜브 동영상 제목, 설명, 댓글 등 필요한 정보 추출
+  // 유튜브 자막 수집
   return {};
 } 
