14b7f31e89f0c8b5062ed1058fcc3f97804076f0
wjsguddn98 <eric1208@naver.com>
2025-06-25 01:46:31 +0900

    add summary collector

---
+++ project/front/src/utils/documentCollector.js
@@ -0,0 +1,5 @@
+
+export function documentCollector() {
+  // TODO: PDF, DOC 등에서 본문 텍스트 추출
+  return {};
+} 
---
+++ project/front/src/utils/youtubeCollector.js
@@ -0,0 +1,4 @@
+export function youtubeCollector() {
+  // TODO: 유튜브 동영상 제목, 설명, 댓글 등 필요한 정보 추출
+  return {};
+} 
