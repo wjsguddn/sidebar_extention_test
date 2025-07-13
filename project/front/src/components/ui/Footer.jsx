658f986dcbe445980dd97ff4bb883bb4b761ccc4
wjsguddn98 <eric1208@naver.com>
2025-07-13 23:27:08 +0900

    footer make

---
+++ project/front/src/components/ui/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+const Footer = ({ children }) => {
+    return (
+      <footer className="footer">
+        {children}
+      </footer>
+    );
+};
+
+export default Footer;
