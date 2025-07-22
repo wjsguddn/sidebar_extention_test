01852bd668ba3a507ad28b4c482c18ace48d5b15
wjsguddn98 <eric1208@naver.com>
2025-07-22 17:03:56 +0900

    documentCollector 구현

--- project/front/src/utils/documentCollector.js
+++ project/front/src/utils/documentCollector.js
@@ -1,4 +1,7 @@
-export function documentCollector() {
-  // PDF, DOC 등에서 본문 텍스트 추출
-  return {};
-} 
+export async function documentCollector(pdfUrl) {
+  console.log('PDF 수집 시작:', pdfUrl);
+  const response = await fetch(pdfUrl);
+  const blob = await response.blob();
+  console.log('Blob 생성 완료:', blob.size, 'bytes');
+  return blob;
+}
