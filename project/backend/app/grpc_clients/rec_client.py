1e7616312eee1cd5e56cfb1601eabe9d74dab3b3
wjsguddn98 <eric1208@naver.com>
2025-08-01 09:35:20 +0900

    edit rec grpc message form

--- project/backend/app/grpc_clients/rec_client.py
+++ project/backend/app/grpc_clients/rec_client.py
@@ -6,13 +6,15 @@ class RecClient:
     def __init__(self, host="llm-worker-rec", port=50051):
         self.target = f"{host}:{port}"
 
-    async def recommend_stream(self, user_id: str, browser_context: str):
+    async def recommend_stream(self, user_id: str, browser_context: str, content_type: str = "default", content_period: str = "none"):
         # 비동기 gRPC 채널 사용
         async with grpc.aio.insecure_channel(self.target) as channel:
             stub = recommendation_pb2_grpc.RecommendationServiceStub(channel)
             request = recommendation_pb2.RecommendRequest(
                 user_id=user_id,
-                browser_context=browser_context
+                browser_context=browser_context,
+                content_type=content_type,
+                content_period=content_period
             )
             # stream 응답을 async generator로 반환
             async for response in stub.Recommend(request):
